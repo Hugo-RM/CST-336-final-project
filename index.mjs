b617b4acab6615e8fbad72a8882061ac317b37a3
Jaime Serrano Acevedo <xilverscott@gmail.com>
2026-04-23 17:42:08 -0700

    Search by name game added as a string api

--- index.mjs
+++ index.mjs
@@ -14,7 +14,13 @@ const steam_retrieve_game_info = 'https://store.steampowered.com/api/appdetails?
 //must add the game ID at the, end link will retrieve game info as  a JSON
 const steam_game_applist = 'https://api.steampowered.com/IStoreService/GetAppList/v1/?key='
 //must add the API key in order to work. Additionally, other parameters can be added to the search
-//for better filtering
+//for better filtering NOTE: that is API is not very updated it
+const steam_search_game_by_name = 'https://store.steampowered.com/api/storesearch/?term='
+//after the original link this string must be added and replace with <SEARCH_QUERY>&l=english&cc=US,
+//in order to work <SEARCH_QUERY> has to be the game name (spaces must be replace with +).
+//for instance when searching for Cyberpunk 2077... term=Cyberpunk+2077&l=engligh ... and will return a
+//JSON that contains mulitple matches and this will include the game name (use in Steam) and the ID
+
 
 const app = express();
 
